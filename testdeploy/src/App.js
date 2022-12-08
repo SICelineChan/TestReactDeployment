ff5d4a028186193cabd8bd58b9d18e9c41821f02
Celine Chan <cctinlone@gmail.com>
2022-12-08 18:52:07 +0100

    Successful Markers!!!!

--- testdeploy/src/App.js
+++ testdeploy/src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
+import React from "react";
+import Map, { Marker } from "react-map-gl";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const mapToken =
+  "pk.eyJ1Ijoibm9vZGxlcGVvcGxlIiwiYSI6ImNsYmR4Z3VxazAyN2kzcG55Nno2bTBtZmMifQ.tMJhZlbKAf53O4lJ82dzAA"; // Set your mapbox token here
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Tell Me If this works!!!!</header>
-    </div>
+    <Map
+      initialViewState={{
+        latitude: 37.8,
+        longitude: -122.4,
+        zoom: 14,
+      }}
+      style={{ width: "100%", height: "100%" }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={mapToken}
+    >
+      <Marker longitude={-122.4} latitude={37.8} color="red" />
+    </Map>
   );
 }
 
